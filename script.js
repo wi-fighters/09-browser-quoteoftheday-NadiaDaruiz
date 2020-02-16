35c1b512e1ae5cd92174649d131e7736657c12c7
Nadia Daruiz <nadiadaruiz@gmail.com>
2020-02-16 23:10:57 +0100

    add more styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (() => {
     const arrayOfQuotes = [
-        "Stay Hungry. Stay Foolish. Steve Jobs", "Good Artists Copy, Great Artists Steal. Pablo Picasso",
-        "Argue with idiots, and you become an idiot. Paul Graham", "Be yourself; everyone else is already taken. Oscar Wilde",
-        "Simplicity is the ultimate sophistication. Leonardo Da Vinci",
-        "Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. Albert Einstein",
-        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. Ralph Emerson", "If you tell the truth, you don't have to remember anything. Mark Twain",
-        "The way get started is to quit talking and begin doing. Walt Disney", "Do What You Can With All You Have, Wherever You Are. Theodore Roosevelt"
+        "Stay Hungry. Stay Foolish. – Steve Jobs", "Good Artists Copy, Great Artists Steal. – Pablo Picasso",
+        "Argue with idiots, and you become an idiot. – Paul Graham", "Be yourself; everyone else is already taken. – Oscar Wilde",
+        "Simplicity is the ultimate sophistication. – Leonardo Da Vinci",
+        "Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe. – Albert Einstein",
+        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. – Ralph Emerson", "If you tell the truth, you don't have to remember anything. – Mark Twain",
+        "The way get started is to quit talking and begin doing. – Walt Disney", "Do What You Can With All You Have, Wherever You Are. – Theodore Roosevelt"
     ];
 
     const button = document.querySelector('.quote-btn');
@@ -17,9 +17,6 @@
     function random() {
         let randomQuote = Math.floor(Math.random() * (arrayOfQuotes.length));
         quoteBox.innerHTML = arrayOfQuotes[randomQuote];
-
         // console.log(quoteBox.innerHTML);
     }
-
-
 })()
